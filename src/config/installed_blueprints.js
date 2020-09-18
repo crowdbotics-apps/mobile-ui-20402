94a8d3855bb259a14bdcb73308c03291bbb59f37
Crowdbotics <team@crowdbotics.com>
2020-09-18 06:12:26 +0000

    add package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'NotificationList8103481', human_name: 'Notification List 8', access_route: 'NotificationList8103481'},
 { name: 'Settings10103479', human_name: 'Settings 10', access_route: 'Settings10103479'},
 { name: 'CalendarView11103478', human_name: 'Calendar View 11', access_route: 'CalendarView11103478', icon: 'calendar'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import NotificationList8103481Navigator from '../features/NotificationList8103481/navigator';
 import Settings10103479Navigator from '../features/Settings10103479/navigator';
 import CalendarView11103478Navigator from '../features/CalendarView11103478/navigator';
 
@@ -15,6 +16,7 @@ import CalendarView11103478Navigator from '../features/CalendarView11103478/navi
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+NotificationList8103481: { screen: NotificationList8103481Navigator },
 Settings10103479: { screen: Settings10103479Navigator },
 CalendarView11103478: { screen: CalendarView11103478Navigator },
 
